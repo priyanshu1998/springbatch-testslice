d9165777295b769f481bf5aaf1a7b8e462ed262f
priyanshu1998 <priyanshutirkey1998@gmail.com>
2024-12-29 20:20:00 +0530

    refactored BatchConfig

--- src/test/java/example/billingjob/configuration/BatchConfig.java
+++ src/test/java/example/billingjob/configuration/BatchConfig.java
@@ -6,38 +6,33 @@ import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.repository.support.ResourcelessJobRepository;
 import org.springframework.batch.support.transaction.ResourcelessTransactionManager;
 import org.springframework.batch.test.StepRunner;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.transaction.PlatformTransactionManager;
-
-import javax.sql.DataSource;
 
 @TestConfiguration
 @EnableBatchProcessing
 public class BatchConfig {
     public static final String CREATE_BILLING_TABLE = """
-		CREATE TABLE IF NOT EXISTS billing_data (
-			data_year INTEGER,
-			data_month INTEGER,
-			account_id INTEGER,
-			phone_number VARCHAR(12),
-			data_usage DOUBLE PRECISION,
-			call_duration INTEGER,
-			sms_count INTEGER
-		);
-	""";
+            	CREATE TABLE IF NOT EXISTS billing_data (
+            		data_year INTEGER,
+            		data_month INTEGER,
+            		account_id INTEGER,
+            		phone_number VARCHAR(12),
+            		data_usage DOUBLE PRECISION,
+            		call_duration INTEGER,
+            		sms_count INTEGER
+            	);
+            """;
 
     public static final String DROP_BILLING_TABLE = """
-        DROP TABLE billing_data IF EXISTS;
-    """;
+                DROP TABLE billing_data IF EXISTS;
+            """;
 
     @Bean
     public StepRunner stepRunner(
             JobLauncher jobLauncher,
-            JobRepository jobRepository){
+            JobRepository jobRepository) {
         return new StepRunner(jobLauncher, jobRepository);
     }
 
